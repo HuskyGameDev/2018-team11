d46e7222157cd082e5a4dfbd16271159a5b4f86f
sjbollno <43500323+sjbollno@users.noreply.github.com>
2019-02-22 18:08:52 -0500

    Revert "Merge branch 'master' of https://github.com/HuskyGameDev/2018-team11"
    This reverts commit e28af1a9adfac84f4a97c28d6e00593a324b4aa4, reversing
    changes made to 951b90b050ad0ddae7554e45ce4fe60386c7de5d.

--- src/lootquest/system/Texture.java
+++ src/lootquest/system/Texture.java
@@ -10,7 +10,7 @@ public class Texture {
 	private BufferedImage attackSheet = null;
 	
 	private BufferedImage[] playerSlash = new BufferedImage[7];
-	BufferedImage[] playerMove = new BufferedImage[12];
+	private BufferedImage[] playerMove = new BufferedImage[7];
 	private BufferedImage[] batMove = new BufferedImage[7];
 	private BufferedImage[] skeletonMove = new BufferedImage[7];
 	
@@ -20,10 +20,10 @@ public class Texture {
 		BufferedImageLoader loader = new BufferedImageLoader();
 		try{
 			//blockSheet = loader.loadImage("/");
-			playerSheet = loader.loadImage("assets/textures/Player/player-move-sheet.png");
+			playerSheet = loader.loadImage("/playerMoveSheet.png");
 			//attackSheet = loader.loadImage("/");
 		}catch(Exception e){
-			e.printStackTrace();
+			
 		}
 		
 		bs = new SpriteSheet(blockSheet);
@@ -37,14 +37,6 @@ public class Texture {
 		playerMove[0] = ps.grabImage(1, 1, 27, 50);
 		playerMove[1] = ps.grabImage(2, 1, 27, 50);
 		playerMove[2] = ps.grabImage(3, 1, 27, 50);
-		playerMove[3] = ps.grabImage(4, 1, 27, 50); 
-		playerMove[4] = ps.grabImage(1, 2, 27, 50); 
-		playerMove[5] = ps.grabImage(2, 2, 27, 50); 
-		playerMove[6] = ps.grabImage(3, 2, 27, 50); 
-		playerMove[7] = ps.grabImage(4, 2, 27, 50); 
-		playerMove[8] = ps.grabImage(1, 3, 27, 50); 
-		playerMove[9] = ps.grabImage(2, 3, 27, 50); 
-		playerMove[10] = ps.grabImage(3, 3, 27, 50); 
-		playerMove[11] = ps.grabImage(4, 3, 27, 50); 
+		playerMove[3] = ps.grabImage(4, 1, 27, 50);
 	}
 }
